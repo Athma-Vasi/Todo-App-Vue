b9b2fc318bf4d777a67ec9a69c51a09ceac8e3c1
Athma Vasi <athma.vasi@protonmail.com>
2023-04-16 19:55:21 -0600

    remove default boilerplate; add tailwind boilerplate

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="text">Hi Athma!!</p>
+  <RouterView />
+</template>
+
+<style scoped>
+.text {
+  @apply text-blue-600 text-2xl;
+}
+</style>
